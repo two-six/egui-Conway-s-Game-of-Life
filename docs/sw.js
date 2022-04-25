0d598592424d218293822a59ae93702d566e0ebb
two-six <twopsix@gmail.com>
2022-04-25 22:18:06 +0200

    Bring back checks

--- docs/sw.js
+++ docs/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './game_of_life.js',
+  './game_of_life_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
